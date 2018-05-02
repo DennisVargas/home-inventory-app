a95e2cddc02051db3034c99555a9022497789596
Dennis_Vargas <dennis.vargas@wsu.edu>
2018-05-02 11:16:16 -0700

    Adds calls to sql database inside main activity to populate screen with items.

--- homeInventory/app/src/main/java/com/vargas/dennis/homeinventory/MainActivity.java
+++ homeInventory/app/src/main/java/com/vargas/dennis/homeinventory/MainActivity.java
@@ -1,5 +1,8 @@
 package com.vargas.dennis.homeinventory;
 
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +11,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private boolean fabExpanded = false;
@@ -20,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
     private ImageView fabOptionsBkgrnd;
     private TextView addItemTextView;
     private TextView addFolderTextView;
+
+    private GridView gridView;
+    private ListView listView;
+    private ArrayList<InventoryItem> list;
+    private InventoryListAdapter adapter = null;
+
+    public static SQLiteHelper sqLiteHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +46,72 @@ public class MainActivity extends AppCompatActivity {
         fab = (FloatingActionButton) findViewById(R.id.fab);
 //        layoutFabAddItem = findViewById(R.id.fabAddItem);
         initFields();
+
+        sqLiteHelper = new SQLiteHelper(this, "HomeInventoryDB.sqlite", null, 1);
+        sqLiteHelper.QueryData("DROP TABLE IF EXISTS INVENTORYITEMS");
+        sqLiteHelper.QueryData("CREATE TABLE IF NOT EXISTS INVENTORYITEMS (name VARCHAR PRIMARY KEY, price VARCHAR, image BLOB)");
+
+        gridView = (GridView) findViewById(R.id.itemGridView);
+        list = new ArrayList<>();
+//        list.add(new InventoryItem(null, "dexterGordon", "12", "1230", null, "Hi this is Dennis", null, null));
+//        list.add(new InventoryItem(null, "dexterGordon", "12", "1230", null, "Hi this is Dennis", null, null));
+//        list.add(new InventoryItem(null, "dexterGordon", "12", "1230", null, "Hi this is Dennis", null, null));
+//        list.add(new InventoryItem(null, "dexterGordon", "12", "1230", null, "Hi this is Dennis", null, null));
+//        list.add(new InventoryItem(null, "dexterGordon", "12", "1230", null, "Hi this is Dennis", null, null));
+        adapter = new InventoryListAdapter(this, R.layout.inventory_item, list);
+        gridView.setAdapter(adapter);
+        AddDatabaseEntry("Disheds", "453", null);
+        AddDatabaseEntry("tOOTHdECAY", "2", null);
+        AddDatabaseEntry("Trom", "2", null);
+        AddDatabaseEntry("ppo", "2", null);
+
+        Cursor cursor = sqLiteHelper.GetData("SELECT * FROM INVENTORYITEMS");
+        list.clear();
+
+        while(cursor.moveToNext()){
+            String name = cursor.getString(0);
+            String price = cursor.getString(1);
+            byte[] image = cursor.getBlob(2);
+
+            list.add(new InventoryItem(image, name,"1", price,null, "notes", null, null ));
+        }
         closeSubMenusFab();
-
     }
-
+    public void AddDatabaseEntry(String name, String price, byte []image){
+        try{
+            sqLiteHelper.InsertData(
+//                    edtName.getText().toString().trim(),
+                    name,
+//                    edtPrice.getText().toString().trim(),
+                    price,
+//                    imageViewToByte(imageView)
+                    image
+            );
+                    Snackbar.make(findViewById(R.id.mainCoordinator), "Added Successfully!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+//            Toast.makeText(getApplicationContext(), "Added successfully!", Toast.LENGTH_SHORT).show();
+//            edtName.setText("");
+//            edtPrice.setText("");
+//            imageView.setImageResource(R.mipmap.ic_launcher);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
     private void initFields() {
         fabAddItem = findViewById(R.id.fabAddItem);
         fabAddFolder = findViewById(R.id.fabAddFolder);
         fabOptionsBkgrnd = findViewById(R.id.greyBgrndImageView);
         addItemTextView = findViewById(R.id.addItemTextView);
         addFolderTextView = findViewById(R.id.addFolderTextView);
+    }
 
+    private byte[] ImageViewToByte(ImageView image){
+        Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
     }
 
     public void ShowFloatingMenuOptions(View view) {
@@ -107,5 +177,16 @@ public class MainActivity extends AppCompatActivity {
         //Change settings icon to 'X' icon
 //        fab.setImageResource(R.drawable.ic_close_black_24dp);
         fabExpanded = true;
+    }
+
+    public void AddItemFabClick(View view){
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
     }
 }
